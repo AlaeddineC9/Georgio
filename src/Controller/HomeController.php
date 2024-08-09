aee66c209968e8cc483bf24e4e9fa1f5221ed571
Alaeddine c9 <alaeddinechraiti@gmail.com>
2024-08-09 16:27:22 +0200

    feat: Update HomeController to pass gallery images to template
    This commit updates the HomeController to retrieve all images from the gallery using the GalerieRepository and pass them to the template. This allows the template to display the images on the home page.
    
    Closes #456

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\GalerieRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route; // Correction de l'import
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function index(GalerieRepository $galerieRepository): Response
     {
+        // Récupérer toutes les images de la galerie
+        $images = $galerieRepository->findAll();
+
+        // Passer les images au template
         return $this->render('home/index.html.twig', [
             'title' => 'Auberge Georgio',
+            'images' => $images,
         ]);
     }
 }
+
