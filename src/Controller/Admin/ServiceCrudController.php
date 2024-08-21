de86223b1fa77607f844259b3dcea5fbbfb175dd
Alaeddine c9 <alaeddinechraiti@gmail.com>
2024-08-21 10:43:24 +0200

    refactor: Update service card styles and layout
    This commit updates the CSS styles and layout for the service cards in the application. It introduces a new class `.service-container` to create a flexbox layout with centered alignment and a gap between the cards. The `.service-card` class is modified to include a position relative, fixed width and height, and a box shadow for a card-like appearance. The image inside the card is set to cover the entire space, and a hover effect is added to reduce the opacity of the image and display a description overlay. These changes enhance the visual presentation and user experience of the service cards.
    
    Closes #456

--- src/Controller/Admin/ServiceCrudController.php
+++ src/Controller/Admin/ServiceCrudController.php
@@ -7,6 +7,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class ServiceCrudController extends AbstractCrudController
 {
@@ -31,5 +34,11 @@ class ServiceCrudController extends AbstractCrudController
                 ->setLabel('Télécharger une nouvelle photo')
                 ->onlyOnForms(), // Pour l'upload dans les formulaires
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 }
--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\Service;
+use App\Repository\ServiceRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class ServicesController extends AbstractController
 {
     #[Route('/services', name: 'services')]
-    public function index(): Response
+    public function index(ServiceRepository $serviceRepository): Response
     {
         return $this->render('services/index.html.twig', [
-            'controller_name' => 'ServicesController',
+            'title' => 'Services',
+            'services' => $serviceRepository->findAll(),
         ]);
     }
 }
